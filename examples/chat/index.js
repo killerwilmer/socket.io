7a91612162cdd87f364a0b8577156fe5994295fd
Wilmer Arteaga <ing.wilmerarteaga@gmail.com>
2016-06-24 10:09:50 -0500

    New port

--- examples/chat/index.js
+++ examples/chat/index.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('../..')(server);
-var port = process.env.PORT || 3000;
+//var port = process.env.PORT || 3000;
+var port = 3001;
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
